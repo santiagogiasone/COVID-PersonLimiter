dac4cdcd4ae3d520898d495de92125f35eedf2d0
Santiago Giasone <santiagogiasone@hotmail.com>
2021-10-17 23:36:08 -0300

    Creacion inicial del proyecto

---
+++ app/src/main/java/com/example/covid_personlimiter/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.covid_personlimiter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
