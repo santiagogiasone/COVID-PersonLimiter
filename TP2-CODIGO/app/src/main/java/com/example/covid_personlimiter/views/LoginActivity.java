977ba699b37caf6584943eddda6844a30cef1ec5
Noorman999 <normanfunes@outlook.com>
2021-10-31 15:24:22 -0300

    Fix LoginActivity

--- TP2-CODIGO/app/src/main/java/com/example/covid_personlimiter/views/LoginActivity.java
+++ TP2-CODIGO/app/src/main/java/com/example/covid_personlimiter/views/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.covid_personlimiter.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,12 +11,14 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.covid_personlimiter.R;
+import com.example.covid_personlimiter.model.UserModel;
 import com.example.covid_personlimiter.presenters.LoginPresenter;
 
 public class LoginActivity extends AppCompatActivity implements LoginViewInterface, View.OnClickListener {
     private EditText editUser;
     private EditText editPass;
     private Button   btnLogin;
+    private Button   btnSignUp;
     private LoginPresenter loginPresenter;
     private ProgressBar progressBar;
 
@@ -28,10 +31,12 @@ public class LoginActivity extends AppCompatActivity implements LoginViewInterfa
         editUser = (EditText) this.findViewById(R.id.mail);
         editPass = (EditText) this.findViewById(R.id.password);
         btnLogin = (Button) this.findViewById(R.id.login);
+        btnSignUp = (Button) this.findViewById(R.id.signup);
         progressBar = (ProgressBar) this.findViewById(R.id.loading);
 
         //set listener
         btnLogin.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
 
         //init
         loginPresenter = new LoginPresenter(this);
@@ -43,7 +48,12 @@ public class LoginActivity extends AppCompatActivity implements LoginViewInterfa
         if (v.getId() == R.id.login){
                 loginPresenter.setProgressBarVisiblity(View.VISIBLE);
                 btnLogin.setEnabled(false);
+                btnSignUp.setEnabled(false);
                 loginPresenter.doLogin(editUser.getText().toString(), editPass.getText().toString());
+        }
+        else if (v.getId() == R.id.signup) {
+            Intent intent=new Intent(LoginActivity.this,SignUpActivity.class);
+            startActivity(intent);
         }
     }
 
@@ -54,11 +64,15 @@ public class LoginActivity extends AppCompatActivity implements LoginViewInterfa
     }
 
     @Override
-    public void onLoginResult(Boolean success, String msg) {
+    public void onLoginResult(Boolean success, String msg, UserModel user) {
         loginPresenter.setProgressBarVisiblity(View.INVISIBLE);
         btnLogin.setEnabled(true);
+        btnSignUp.setEnabled(true);
         if (success){
             Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+            intent.putExtra("user", user);
+            startActivity(intent);
         }
         else
             Toast.makeText(this, msg,Toast.LENGTH_SHORT).show();
